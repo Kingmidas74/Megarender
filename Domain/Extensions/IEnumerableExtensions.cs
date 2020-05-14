b8a624bcce3b6202a14477028808ac4960bab3f0
Suleymanov Denis <kingmidas1992@gmail.com>
2020-05-14 16:03:54 +0300

    Add ForEach for IEnumerable

--- Domain/Extensions/IEnumerableExtensions.cs
+++ Domain/Extensions/IEnumerableExtensions.cs
@@ -7,5 +7,14 @@ namespace Megarender.Domain.Extensions {
         public static bool HaveDuplicates<TEntity, TProperty> (this IEnumerable<TEntity> list, Func<TEntity, TProperty> expression) {
             return list.Count () != list.Select (expression).Distinct ().Count ();
         }
+
+        public static IEnumerable<TEntity> ForEach<TEntity>(this IEnumerable<TEntity> list, Action<TEntity> action)
+        {            
+            foreach(var element in list)
+            {
+                action(element);
+            }
+            return list;
+        }
     }
 }
